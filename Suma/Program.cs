79b9aa35d801c67f1d23c4a4f9604b1b0a326e76
fbarrientos <fbarrientos@uabc.edu.mx>
2022-03-01 16:02:55 -0800

    Add project files.

---
+++ Suma/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 8;
+            double num2 = 4.5;
+            double resultado;
+
+            //Variable string 
+            string saludo = "Hola";
+            string nombre = "Hugo";
+            //Sumamos las dos variables
+            //resultado = num1 + num2;
+
+            //Sumando una variable y un dato puesto explicitamente
+            resultado = num1 + 22;
+            //Console.WriteLine(resultado);
+
+
+            //Sumando o concatenando dos cadenas almacenadas en variables 
+            Console.WriteLine(saludo + " " + nombre + " " + "Como estas?");
+            Console.ReadKey();
+        }
+    }
+}
